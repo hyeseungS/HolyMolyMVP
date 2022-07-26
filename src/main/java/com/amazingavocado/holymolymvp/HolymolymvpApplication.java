31b65af0b5177c6da4f80806a68107e036dcbb7a
신혜승 <hyeseung2000@gmail.com>
2022-07-26 17:55:25 +0900

    Initial commit

---
+++ src/main/java/com/amazingavocado/holymolymvp/HolymolymvpApplication.java
@@ -0,0 +1,13 @@
+package com.amazingavocado.holymolymvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolymolymvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolymolymvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amazingavocado/holymolymvp/HolymolymvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingavocado.holymolymvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolymolymvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
