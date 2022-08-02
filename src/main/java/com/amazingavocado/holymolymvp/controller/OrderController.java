f9c35b7508048d9b9cc7a5042550c01dd5d9580e
Juyeon999 <19j@sookmyung.ac.kr>
2022-08-02 18:50:57 +0900

    꽃상태주의팝업, 주문정보, 주문자정보, 예약팝업, 예약완료 thymeleaf 적용, 프론트 세부완성

---
+++ src/main/java/com/amazingavocado/holymolymvp/controller/OrderController.java
@@ -0,0 +1,49 @@
+package com.amazingavocado.holymolymvp.controller;
+
+import com.amazingavocado.holymolymvp.dto.orderForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class OrderController {
+
+    //예약안내 페이지(modal)
+    @GetMapping("/order-start")
+    public String orderStart() {
+        return "alert_start";
+    }
+
+
+    //주문정보 페이지
+    @GetMapping("/order")
+    public String order() {
+        //templates 폴더의 "order_info"파일이 열린다.
+        return "order_info";
+    }
+
+    @PostMapping("/order")
+    public String neworder(orderForm form) {
+        System.out.println(form.toString());
+        return "";
+    }
+
+    //주문자정보 페이지
+    @PostMapping("/orderer")
+    public String orderer() {
+        return "orderer_info";
+    }
+
+    //전송하기 페이지(modal)
+    @PostMapping("/order-send")
+    public String sendorder() {
+        return "alert_send";
+    }
+
+    //예약요청완료 페이지
+    @PostMapping("/order-complete")
+    public String orderComplete() {
+        return "order_complete";
+    }
+
+}
