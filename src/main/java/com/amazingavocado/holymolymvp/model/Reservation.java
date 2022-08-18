abdcd8c967779cabc144e0a274ac327fa49a464a
Juyeon999 <19j@sookmyung.ac.kr>
2022-08-19 08:15:53 +0900

    [ing] 픽업 배송 수정중

--- src/main/java/com/amazingavocado/holymolymvp/model/Reservation.java
+++ src/main/java/com/amazingavocado/holymolymvp/model/Reservation.java
@@ -62,6 +62,14 @@ public class Reservation extends Timestamped {
     @Column(name = "reservation_price", nullable = false)
     private String reservationPrice;
 
+    //수령 방식
+    @Column (name = "reservation_way", nullable = false)
+    private String reservationWay;
+
+    //배달일 경우- 수령주소
+    @Column (name = "reseration_delivery_address")
+    private String reservationDeliveryAddress;
+
     // 픽업 날짜
     @Column(name = "reservation_pickup_date", nullable = false)
     private String reservationPickupDate;
